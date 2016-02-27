fad9c0e51806de8e20b045f4957b35ec68f994ae
Niko <niko.caron90@gmail.com>
2016-02-26 21:05:03 -0400

    Grunt to Gulp
    Switched from Grunt to Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+
+	gulp.watch('index.html').on('change', browserSync.reload);
+	gulp.watch('css/*.css').on('change', browserSync.reload);
+	gulp.watch('js/*.css').on('change', browserSync.reload);
+});
