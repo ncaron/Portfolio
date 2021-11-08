699e02ee8d6a4e9e889fc131aefbabed2187f8e4
Niko Caron <niko.caron90@gmail.com>
2021-11-08 02:23:32 +0100

    Fix string escape

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -13,10 +13,11 @@ const About: NextPage = () => {
       <Photo />
 
       <p className={styles.aboutMe}>
-        Hi, I'm Niko, a web developer from Canada but living in Berlin, I
+        Hi, I&apos;m Niko, a web developer from Canada but living in Berlin, I
         relocated here for my first ever software job and loving every bit of it
-        so far! Coding is my passion, I can't stop thinking about it and I'm
-        excited to continue my career as a frontend/fullstack developer.
+        so far! Coding is my passion, I can&apos;t stop thinking about it and
+        I&apos;m excited to continue my career as a frontend/fullstack
+        developer.
       </p>
 
       <Icon />
